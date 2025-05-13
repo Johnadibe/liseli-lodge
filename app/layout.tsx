4a83e182cfdd9a6e64f514345cbad43dd1adac2e
unknown <johnadibe123@gmail.com>
2025-05-13 02:20:27 +0100

    Add globals style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter as FontSans } from "next/font/google"
-import "@/styles/globals.css"
+import "./globals.css"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
 import Navbar from "@/components/navbar"
